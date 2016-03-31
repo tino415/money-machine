dd87432badc15bae5575f8640f111f3f72147817
tino415 <cernakmartin3@gmail.com>
2016-03-31 18:23:20 +0200

    Fix read to table

--- concat.R
+++ concat.R
@@ -1,8 +1,9 @@
 #!/usr/bint/env Rscript
 
 read_data <- function() {
+	#TODO Sort file names alpha
 	files = list.files(path='data/raw', patter='*.csv', full.names=TRUE)
 
-	return(lapply(files, read.csv))
+	return(lapply(files, read.csv, sep = ';'))
 }
 
