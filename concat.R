7f716f2cab5d192c90199db38c224511ea90c9a1
tino415 <cernakmartin3@gmail.com>
2016-03-31 18:30:32 +0200

    Fix file ordering

--- concat.R
+++ concat.R
@@ -1,8 +1,7 @@
 #!/usr/bint/env Rscript
 
 read_data <- function() {
-	#TODO Sort file names alpha
-	files = list.files(path='data/raw', patter='*.csv', full.names=TRUE)
+	files = sort(list.files(path='data/raw', patter='*.csv', full.names=TRUE), decreasing = TRUE)
 
 	return(lapply(files, read.csv, sep = ';'))
 }
