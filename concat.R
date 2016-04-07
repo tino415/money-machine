82b9a857478f2eed5bac8e0c0e991ce226aa5f30
tino415 <cernakmartin3@gmail.com>
2016-04-07 17:40:34 +0200

    Data as frame

--- concat.R
+++ concat.R
@@ -3,7 +3,7 @@
 read_data <- function() {
 	files = sort(list.files(path='data/raw', patter='*.csv', full.names=TRUE), decreasing = TRUE)
 
-	data = as.matrix((lapply(files, read.csv, sep = ';'))[[1]])
+	data = (lapply(files, read.csv, sep = ';'))[[1]]
 
 	colnames(data) <- c("datetime", "open", "high", "low", "close", "return")
 
