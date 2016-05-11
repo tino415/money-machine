a7bc43c0093c8ad4d6b6b507f78e24ed5141bf89
tino415 <cernakmartin3@gmail.com>
2016-05-11 21:00:09 +0200

    Neural network

--- network.R
+++ network.R
@@ -1,4 +1,22 @@
 #!/usr/bin/env Rscript
+library(neuralnet)
 
-forecast <- function() {
+forecast <- function(data) {
+
+	prev = 0
+
+	train <- data[1:40,]
+
+	# Musí obsahovať iba vstupné parametre
+	test <- data[20:100, c("open", "high", "low", "close")]
+
+	for (i in 2:nrow(train)) { train[i, 'bull'] = train[i, 'close'] > train[i-1, 'close'] }
+
+	nn <- neuralnet(bull ~ open + high + low + close, train, hidden = c(3,5))
+
+	res <- compute(nn, test)
+
+	d <- apply(res$net.result, 1, function(d) { return(round(d)) })
+
+	return(d)
 }
